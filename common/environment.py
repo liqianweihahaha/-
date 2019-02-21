1fb01a727c7912a4453c8874b901232e8f0f3eee
maxiaoqian <maxiaoqian199202@foxmail.com>
2019-02-21 14:10:45 +0800

    增加数据库环境配置&增加压测环境数据

--- common/environment.py
+++ common/environment.py
@@ -2,7 +2,8 @@
 def get_hosts(env):
     if env == 'dev':
         tiger_api_host = 'https://backend-dev.codemao.cn'
-        platform_tiger_api_host = 'http://dev-external.platform.codemao.cn'
+        # platform_tiger_api_host = 'http://dev-external.platform.codemao.cn'
+        platform_tiger_api_host = 'http://dev-account.internal.platform.codemao.cn'
     elif env == 'test':
         tiger_api_host = "https://test-api.codemao.cn"
         platform_tiger_api_host = 'http://test-internal.platform.codemao.cn'
@@ -12,6 +13,9 @@ def get_hosts(env):
     elif env == 'production':
         tiger_api_host = 'https://api.codemao.cn'
         platform_tiger_api_host = 'http://internal.platform.codemao.cn'
+    elif env == 'press':
+        tiger_api_host = 'https://press-api.codemao.cn'
+        platform_tiger_api_host = 'http://press-internal.platform.codemao.cn'
     # 预演环境
     elif env == 'preview':
         tiger_api_host = 'https://preview-api.codemao.cn'
