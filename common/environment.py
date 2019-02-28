4a9db795a2e0093cd3a28c97073a101cad4f2fcf
maxiaoqian <maxiaoqian199202@foxmail.com>
2019-02-28 11:28:40 +0800

    增加作品workcore测试用例

--- common/environment.py
+++ common/environment.py
@@ -2,7 +2,7 @@
 def get_hosts(env):
     if env == 'dev':
         tiger_api_host = 'https://backend-dev.codemao.cn'
-        platform_tiger_api_host = 'http://dev-external.platform.codemao.cn'
+        platform_tiger_api_host = 'http://dev-internal.platform.codemao.cn'
     elif env == 'test':
         tiger_api_host = "https://test-api.codemao.cn"
         platform_tiger_api_host = 'http://test-internal.platform.codemao.cn'
