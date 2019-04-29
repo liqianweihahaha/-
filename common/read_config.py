52a400b34d746f4a0e2c748dac1decd0cd514e9a
maxiaoqian <maxiaoqian199202@foxmail.com>
2019-04-29 17:01:18 +0800

    简化read_config.py

--- common/read_config.py
+++ common/read_config.py
@@ -4,41 +4,39 @@ import os
 conf_dir = os.path.join(os.path.dirname(__file__), '..', 'config')
 
 # 读取用户配置
-def read_user(file_name, file_dir=conf_dir):
+def read_user(env, file_dir=conf_dir):
+    file_name = str(env) + '_user.yaml'
     file_path = os.path.join(file_dir, 'user', file_name)
     with open(file_path, 'r', encoding='utf-8') as f:
         data = yaml.load(f)
         return data
 
 def source_user(env):
-    file_name = str(env)+'_user.yaml'
-    source_user_data = read_user(file_name)['source_user']
+    source_user_data = read_user(env)['source_user']
     return source_user_data
 
 def target_user(env):
-    file_name = str(env)+'_user.yaml'
-    target_user_data = read_user(file_name)['target_user']
+    target_user_data = read_user(env)['target_user']
     return target_user_data
 
 # 读取数据库配置
-def read_db(file_name, file_dir=conf_dir):
+def read_db(env, file_dir=conf_dir):
+    file_name = str(env) + '_db.yaml'
     file_path = os.path.join(file_dir, 'db', file_name)
     with open(file_path, 'r', encoding='utf-8') as f:
         data = yaml.load(f)
         return data
 
 def read_config_mysql(env):
-    file_name = str(env)+'_db.yaml'
-    mysql_data = read_db(file_name)['mysql']
+    mysql_data = read_db(env)['mysql']
     return mysql_data
 
 def read_config_redis(env):
-    file_name = str(env)+'_db.yaml'
-    redis_data = read_db(file_name)['redis']
+    redis_data = read_db(env)['redis']
     return redis_data
 
 
 if __name__ == '__main__':
-    user = read_user('user.yaml')
     env = 'dev'
-    print(user[env]['source_user'])
+    user = read_user(env)
+    print(user['source_user'])
